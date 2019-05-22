31ef6f336d3d66e5cb16423b2a97592a8d6a4a01
Łukasz Kwiecień <lukaskwkw@gmail.com>
2019-05-22 22:30:42 +0200

    Add styled-components

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,10 +1,26 @@
 import React from "react";
+import styled from "styled-components";
+
 import Header from "../components/Header";
 import Link from "../components/Link";
 
+const Images = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+const ImageContainer = styled.div`
+  max-width: 240px;
+  width: 100%;
+  height: 100px;
+  flex: 1 0 21%;
+  background-image: url("https://lorempixel.com/240/100/");
+`;
+
 const TestPug = () => pug`
-  .testClass
-    p.innaKlasa Good #[strong Morning]
+  Images
+    each image in Array(5).fill()
+      ImageContainer(key=image)
 `;
 
 const Portfolio = () => (
