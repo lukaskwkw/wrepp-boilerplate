ba8f79f14d3f0c2e3feb7faa0406670d5892160a
Łukasz Kwiecień <lukaskwkw@gmail.com>
2019-05-22 21:29:46 +0200

    Add pug template language support

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -2,9 +2,15 @@ import React from "react";
 import Header from "../components/Header";
 import Link from "../components/Link";
 
+const TestPug = () => pug`
+  .testClass
+    p.innaKlasa Good #[strong Morning]
+`;
+
 const Portfolio = () => (
   <>
     <Header>Portfolio section</Header>
+    <TestPug />
     <Link href="/">Main Page</Link>
   </>
 );
